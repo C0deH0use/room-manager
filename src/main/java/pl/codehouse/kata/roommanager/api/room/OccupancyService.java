c85fb70ff3e26d35a73ed41ee060546d7dad4445
Marek Malik <info@code-house.pl>
2023-11-05 17:35:12 +0100

    fix: move remaining dto to correct package

--- src/main/java/pl/codehouse/kata/roommanager/api/room/OccupancyService.java
+++ src/main/java/pl/codehouse/kata/roommanager/api/room/OccupancyService.java
@@ -1,6 +1,7 @@
 package pl.codehouse.kata.roommanager.api.room;
 
 import pl.codehouse.kata.roommanager.api.room.dto.RoomOccupancyDto;
+import pl.codehouse.kata.roommanager.api.room.dto.RoomsAvailabilityDto;
 
 public interface OccupancyService {
     RoomOccupancyDto checkInGuests(RoomsAvailabilityDto roomsAvailabilityDto);
--- src/main/java/pl/codehouse/kata/roommanager/api/room/RoomResource.java
+++ src/main/java/pl/codehouse/kata/roommanager/api/room/RoomResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.codehouse.kata.roommanager.api.room.dto.RoomOccupancyDto;
+import pl.codehouse.kata.roommanager.api.room.dto.RoomsAvailabilityDto;
 
 @Validated
 @RestController
